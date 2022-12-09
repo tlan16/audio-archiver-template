e1035797b9162d49864d76344ae41d7fadd2ab32
Frank Lan <franklan118@gmail.com>
2022-12-09 15:19:04 +1100

    install yt-dlp

--- download.sh
+++ download.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-PROJECT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 cd "$PROJECT_DIR"
 
 #URL="https://space.bilibili.com/316568752"
 URL="https://www.bilibili.com/video/BV1dR4y1Q7zV"
 
 yt-dlp \
+  --verbose \
   --ignore-errors \
   --sleep-interval 1 \
   --max-sleep-interval 5 \
