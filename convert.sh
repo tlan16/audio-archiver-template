4a90137f7a6241f0ac4898a363293a2795c6d0a3
Frank Lan <franklan118@gmail.com>
2022-12-09 16:14:41 +1100

    nicer message

--- convert.sh
+++ convert.sh
@@ -8,6 +8,11 @@ INPUT_FILE="$1"
 TEMP_FILE="$(uuidgen).m4a"
 DOCKER_IMAGE="jrottenberg/ffmpeg:5.1.2-alpine313"
 
+if [[ "$(docker images -q myimage:mytag 2> /dev/null)" == "" ]]; then
+  echo "Docker image \"${DOCKER_IMAGE}\" not found on local. Pull..."
+  docker pull --quiet "$DOCKER_IMAGE"
+fi
+
 echo "Converting $INPUT_FILE to $TEMP_FILE"
 
 docker run -v "$PROJECT_DIR":"$PROJECT_DIR" -w "$PROJECT_DIR" "$DOCKER_IMAGE" \
