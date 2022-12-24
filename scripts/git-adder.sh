6b8ce80a3935ac82a27da00f25c1f98100a9b88d
Frank Lan <franklan118@gmail.com>
2022-12-24 01:05:22 +0000

    Update git-adder.sh

--- scripts/git-adder.sh
+++ scripts/git-adder.sh
@@ -1,17 +1,45 @@
 #!/usr/bin/env bash
 # This is experimental.
 
-set -euo pipefail
+set -euox pipefail
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 PROJECT_DIR="$SCRIPT_DIR/.."
 cd "$PROJECT_DIR" || exit 1
 
 # Set the maximum size in bytes
-max_size=20000000 # 20 MB
+max_size=40000000 # 40 MB
+split_threshold=50000000 # 50 MB
 
 # Initialize the total size to 0
 total_size=0
+
+function splitFile() {
+  local path dir fileName
+  path="$(realpath "$1")"
+  fileName=$(basename -- "$path")
+  dir="${path%%.*}"
+  mkdir "$dir"
+  cd "$dir" || exit 1
+  mv "$path" "${dir}/${fileName}"
+  7z -v10m -mx=9 a "${fileName}.7z" "$fileName" > /dev/null
+  rm -f "$fileName"
+  cd ..
+}
+
+# shellcheck disable=SC2044
+for file in *.* **/*; do
+  if [ ! -f "$file" ]; then
+    continue
+  fi
+
+  file_size=$(wc -c <"$file")
+  if [ "$file_size" -lt "$split_threshold" ]; then
+    continue
+  fi
+
+  splitFile "$file"
+done
 
 # shellcheck disable=SC2044
 for file in *.* **/*; do
@@ -19,7 +47,6 @@ for file in *.* **/*; do
     continue
   fi
 
-  file_size=$(wc -c <"$file")
   if [ $total_size -gt $max_size ] && [ "$(git status --porcelain)" ]; then
     git commit --message "$total_size"
     git push --force-with-lease
