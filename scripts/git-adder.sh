e7a82cc929f261ff8c78455fe282d4e23227395f
Frank Lan <franklan118@gmail.com>
2022-12-29 01:30:50 +0000

    Update git-adder.sh

--- scripts/git-adder.sh
+++ scripts/git-adder.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # This is experimental.
 
-set -euox pipefail
+set -euo pipefail
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 PROJECT_DIR="$SCRIPT_DIR/.."
