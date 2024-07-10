29274223d241c322218bdeaa4c252d3b511197c8
Frank Lan <franklan118@gmail.com>
2024-07-10 02:08:30 +0000

    Update git-adder.sh

--- scripts/git-adder.sh
+++ scripts/git-adder.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 shopt -s globstar
 set -euo pipefail
+#set -x
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 PROJECT_DIR="$SCRIPT_DIR/.."
@@ -26,34 +27,37 @@ function human_size() {
 
 # shellcheck disable=SC2044
 for file in ./* ./**/*; do
-  echo "$file"
-	if [[ "$file" == *"URLs.txt" ]] || [ ! -f "$file" ]; then
-    echo "Skipped."
-		continue
-  fi
-  file_size=$(get_File_file_bytes "$file")
-  staged_files_count=$(git diff --cached --numstat | wc -l)
+  echo "Path: $file";
+	if [ -f "$file" ]; then
+	  echo "File: $file";
+	  file_size=$(get_File_file_bytes "$file");
+    staged_files_count=$(git diff --cached --numstat | wc -l);
+    echo "File size: $file_size";
+    echo "Staged files count: $staged_files_count";
 
-  if [ $file_size -gt 90000000 ]; then
-    echo "Skipping ${file} because it is larger than 90MB."
-    continue
-  fi
+    if [ "$file_size" -lt 90000000 ]; then
+      if git check-ignore -q "$file"; then
+        echo "Skipped git ignored file."
+      else
+        if [ $total_size -gt $max_size ] || [ $staged_files_count -gt 100 ] ; then
+          echo "Commiting and pushing.";
+          git commit --message "$total_size" > /dev/null 2>&1 || true;
+          git push > /dev/null 2>&1 || true;
+          total_size=0;
+          echo "Commited and pushed";
+        fi
 
-  if [ $total_size -gt $max_size ] || [ $staged_files_count -gt 100 ] ; then
-    git commit --message "$total_size"
-    git push
-		total_size=0
-  fi
-
-  if git check-ignore -q "$file"; then
-    echo "Skipped"
-		continue
-  fi
-
-  if git diff --cached --quiet "$file"; then
-    git add "$file"
-		total_size=$((total_size + file_size))
-		echo "Added ${file}. $(human_size "$total_size")/$(human_size "$max_size"), $(git diff --cached --numstat | wc -l)/100."
+        if git diff --cached --quiet "$file"; then
+          git add "$file";
+          total_size=$((total_size + file_size));
+          echo "Added ${file}. $(human_size "$total_size")/$(human_size "$max_size"), $(git diff --cached --numstat | wc -l)/100.";
+        fi
+      fi
+    else
+      echo "Skipping ${file} because it is larger than 90MB."
+    fi
+	else
+    echo "Skipped non file path."
   fi
 done
 
